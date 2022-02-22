1e4ed34a55f91ac19e4f9fa1a0731f1ceb0ef9ca
Mikail Gökce <66737755+themikail@users.noreply.github.com>
2022-02-22 21:16:50 +0100

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,15 +2,15 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDdFA24PRCvzquxVt2zhZEZiub11wjmRlA",
-  authDomain: "limon-bef96.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   databaseURL:
-    "https://limon-bef96-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "limon-bef96",
-  storageBucket: "limon-bef96.appspot.com",
-  messagingSenderId: "858857208906",
-  appId: "1:858857208906:web:a79d2118e46a609b0a3ab3",
-  measurementId: "G-F4L52T54M3",
+    "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 });
 
 export const auth = app.auth();
